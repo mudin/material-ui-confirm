042daa688ede26c466c6fa51bff3725bcfaa55e2
Kevin <kevin@plang.ai>
2021-06-25 16:18:48 +0900

    add code

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { action } from "@storybook/addon-actions";
 import Button from "@material-ui/core/Button";
 import LinearProgress from "@material-ui/core/LinearProgress";
@@ -6,6 +6,7 @@ import Box from "@material-ui/core/Box";
 import Tooltip from "@material-ui/core/Tooltip";
 import { storiesOf, addDecorator } from "@storybook/react";
 import { ConfirmProvider, useConfirm } from "../src/index";
+import ConfirmationDialog from "../src/ConfirmationDialog";
 
 const confirmationAction = action("confirmed");
 
@@ -138,6 +139,68 @@ const WithCustomContent = () => {
   );
 };
 
+const Dialog = ({ annotationFetchStatus }) => {
+  return (
+    <div>
+      <LinearProgress />
+      <Box p={2}>
+        {annotationFetchStatus === "Pending"
+          ? "working..."
+          : "This isn't wrapped in DialogContentText."}
+      </Box>
+    </div>
+  );
+};
+
+const WithLoading = () => {
+  // const confirm = useConfirm();
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [annotationFetchStatus, setAnnotationFetchStatus] = useState("Wait");
+
+  const __closeDialog = useCallback(() => {
+    setIsDialogOpen(!isDialogOpen);
+  }, [isDialogOpen]);
+
+  const __onConfirm = useCallback(() => {
+    setAnnotationFetchStatus("Pending");
+
+    // do any work after
+    setTimeout(() => {
+      __closeDialog();
+      setAnnotationFetchStatus("Wait");
+    }, 3000);
+  }, [__closeDialog]);
+
+  useEffect(() => {
+    if (annotationFetchStatus === "Pending") {
+      setLoading(true);
+    } else {
+      setLoading(false);
+    }
+
+    return () => {};
+  }, [annotationFetchStatus, loading]);
+
+  return (
+    <div>
+      <Button onClick={() => setIsDialogOpen(true)}>Click</Button>
+      <ConfirmationDialog
+        open={isDialogOpen}
+        options={{
+          confirmationLoading: loading,
+          content: <Dialog annotationFetchStatus={annotationFetchStatus} />,
+          confirmationText: "confirm",
+          cancellationText: "Cancel",
+        }}
+        onConfirm={__onConfirm}
+        onCancel={__closeDialog}
+        onClose={__closeDialog}
+      />
+    </div>
+  );
+};
+
 storiesOf("Confirmation dialog", module)
   .addDecorator((getStory) => <ConfirmProvider>{getStory()}</ConfirmProvider>)
   .add("basic", () => <Basic />)
@@ -147,4 +210,5 @@ storiesOf("Confirmation dialog", module)
   .add("with custom button props", () => <WithCustomButtonProps />)
   .add("with custom callbacks", () => <WithCustomCallbacks />)
   .add("with custom elements", () => <WithCustomElements />)
-  .add("with custom dialog content", () => <WithCustomContent />);
+  .add("with custom dialog content", () => <WithCustomContent />)
+  .add("with loading", () => <WithLoading />);
